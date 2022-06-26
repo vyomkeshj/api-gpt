759461662b61be5076759c31bf1c5ed22c6d53a7
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-26 14:03:13 +0200

    Updated requirements

--- serve.py
+++ serve.py
@@ -33,7 +33,6 @@ per_replica_batch = params["per_replica_batch"]
 cores_per_replica = params["cores_per_replica"]
 seq = params["seq"]
 
-
 params["sampler"] = nucleaus_sample
 
 # here we "remove" the optimizer parameters from the model (as we don't need them for inference)
@@ -42,7 +41,7 @@ params["optimizer"] = optax.scale(0)
 mesh_shape = (jax.device_count() // cores_per_replica, cores_per_replica)
 devices = np.array(jax.devices()).reshape(mesh_shape)
 
-maps.thread_resources.env = maps.ResourceEnv(maps.Mesh(devices, ("dp", "mp")),())
+maps.thread_resources.env = maps.ResourceEnv(maps.Mesh(devices, ("dp", "mp")), ())
 
 tokenizer = transformers.GPT2TokenizerFast.from_pretrained("gpt2")
 
@@ -53,19 +52,82 @@ network.state = read_ckpt(network.state, "./step_201/", devices.shape[1])
 del network.state["opt_state"]
 network.state = network.move_xmap(network.state, np.zeros(cores_per_replica))
 
+header = """Take the database columns in the [Schema Json] section, question in [Question] section and generate SQL for the question on the schema."""
 
+schema = """ insurance_data:[
+  months_as_customer,
+  age,
+  policy_number,
+  policy_bind_date,
+  policy_state,
+  policy_csl,
+  policy_deductable,
+  policy_annual_premium,
+  umbrella_limit,
+  insured_zip,
+  insured_sex,
+  insured_education_level,
+  insured_occupation,
+  insured_hobbies,
+  insured_relationship,
+  capital_gains,
+  capital_loss,
+  incident_date,
+  incident_type,
+  collision_type,
+  incident_severity,
+  authorities_contacted,
+  incident_state,
+  incident_city,
+  incident_location,
+  incident_hour_of_the_day,
+  number_of_vehicles_involved,
+  property_damage,
+  bodily_injuries,
+  witnesses,
+  police_report_available,
+  total_claim_amount,
+  injury_claim,
+  property_claim,
+  vehicle_claim,
+  auto_make,
+  auto_model,
+  auto_year,
+  fraud_reported
+  ]
+"""
+
+kwargs = {'min_length': 0,
+          'max_length': None,
+          'length_no_input': True,
+          'remove_input': True,
+          'top_p': 0.95,
+          'temperature': 0.90,
+          'top_k': 50,
+          'repetition_penalty': 1.5,
+          'length_penalty': 1.2,
+          'do_sample': True,
+          'early_stopping': True,
+          'num_beams': 5,
+          'no_repeat_ngram_size': 0,
+          'num_return_sequences': 1,
+          'remove_end_sequence': True}
+
+context_initial = f"{header} {schema}"
 @app.post("/generate")
 async def generate(
-    context: Optional[
-        str
-    ] = "In a shocking finding, scientists discovered a herd of unicorns living in a remote, previously unexplored valley, in the Andes Mountains. Even more surprising to the researchers was the fact that the unicorns spoke perfect English.",
-    token_max_length: Optional[int] = 512,
-    temperature: Optional[float] = 1.0,
-    top_p: Optional[float] = 0.9,
-    stop_sequence: Optional[str] = None,
+        question: Optional[
+            str
+        ] = "",
+        token_max_length: Optional[int] = 512,
+        temperature: Optional[float] = 1.0,
+        top_p: Optional[float] = 0.9,
+        stop_sequence: Optional[str] = None,
 ):
     start = time.time()
-    tokens = tokenizer.encode(context)
+    input = f"{context_initial} Question: {question} SQL: "
+
+    tokens = tokenizer.encode(input)
     provided_ctx = len(tokens)
     if token_max_length + provided_ctx > 2048:
         return {"text": "Don't abuse the API, please."}
@@ -79,10 +141,7 @@ async def generate(
         batched_tokens,
         length,
         token_max_length,
-        {
-            "top_p": np.ones(total_batch) * top_p,
-            "temp": np.ones(total_batch) * temperature,
-        },
+        **kwargs,
     )
 
     text = tokenizer.decode(output[1][0][0, :, 0])
@@ -92,10 +151,10 @@ async def generate(
         text = text.split(stop_sequence)[0] + stop_sequence
 
     response = {}
-    response["model"] = "GPT-J-6B"
+    response["model"] = "gpt-sql"
     response["compute_time"] = time.time() - start
     response["text"] = text
-    response["prompt"] = context
+    response["prompt"] = question
     response["token_max_length"] = token_max_length
     response["temperature"] = temperature
     response["top_p"] = top_p
@@ -105,5 +164,5 @@ async def generate(
     return response
 
 
-print("GPT-J-6B serving!")
+print("sql-model-serving")
 uvicorn.run(app, host="0.0.0.0", port=5000)
