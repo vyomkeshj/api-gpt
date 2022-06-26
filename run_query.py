55ca95c39a22c3113adcad44c3c7d3ed51e3198c
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-26 15:10:05 +0200

    run query

---
+++ run_query.py
@@ -0,0 +1,37 @@
+import argparse
+import requests
+
+def parse_args():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--question", type=str, help="Your Query.")
+    args = parser.parse_args()
+    return args
+
+def find_between(s, first, last):
+    try:
+        start = s.index( first ) + len( first )
+        end = s.index( last, start )
+        return s[start:end]
+    except ValueError:
+        return ""
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    question_on_insurance = args.question
+
+    payload = {
+        "question": question_on_insurance,
+        "token_max_length": 340,
+        "temperature": 0.90,
+        "top_p": 0.95,
+    }
+
+    response = requests.post("http://localhost:5000/generate", params=payload).json()
+    response  = response['text']
+    before, sep, after = response.partition('SELECT')
+    query = sep + after
+    query = find_between(query, "SELECT", "###")
+    query = "SELECT" + query
+    print("Query: " + query + '\n')
