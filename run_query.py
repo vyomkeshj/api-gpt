2bf2a839ce466a2e9ae4cc7a2004aba828dd3db4
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-27 04:53:37 +0200

    seed

--- run_query.py
+++ run_query.py
@@ -23,18 +23,19 @@ if __name__ == "__main__":
     args = parse_args()
 
     question_on_insurance = args.question
-    print(f"{i} \n")
-    payload = {
-        "question": question_on_insurance,
-        "token_max_length": 340,
-        "temperature": 0.655172,
-        "top_p": 0.95,
-    }
+    for i in np.linspace(0.2, 1.3, 20):
+        print(f"{i} \n")
+        payload = {
+            "question": question_on_insurance,
+            "token_max_length": 340,
+            "temperature": 0.655172,
+            "top_p": 0.95,
+        }
 
-    response = requests.post("http://localhost:5000/generate", params=payload).json()
-    response = response['text']
-    before, sep, after = response.partition('SELECT')
-    query = sep + after
-    query = find_between(query, "SELECT", "###")
-    query = "SELECT" + query
-    print("Query: " + query + '\n')
+        response = requests.post("http://localhost:5000/generate", params=payload).json()
+        response = response['text']
+        before, sep, after = response.partition('SELECT')
+        query = sep + after
+        query = find_between(query, "SELECT", "###")
+        query = "SELECT" + query
+        print("Query: " + query + '\n')
