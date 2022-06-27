a2bfb77568d48e53c4a2021b319a26064dc4d250
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-27 05:00:43 +0200

    seed

--- run_query.py
+++ run_query.py
@@ -23,14 +23,15 @@ if __name__ == "__main__":
     args = parse_args()
 
     question_on_insurance = args.question
-    for i in np.linspace(0.2, 1.3, 20):
-        print(f"{i} \n")
-        payload = {
-            "question": question_on_insurance,
-            "token_max_length": 340,
-            "temperature": 0.655172,
-            "top_p": 0.95,
-        }
+    for p in np.linspace(0.70, 1.0, 10):
+        for t in np.linspace(0.2, 0.9, 10):
+            print(f"{t} \n")
+            payload = {
+                "question": question_on_insurance,
+                "token_max_length": 340,
+                "temperature": t,
+                "top_p": p,
+            }
 
         response = requests.post("http://localhost:5000/generate", params=payload).json()
         response = response['text']
