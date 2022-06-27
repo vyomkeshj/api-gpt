fbb159f91c0e5588d5f945dc59f17338098118a9
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-27 05:18:22 +0200

    seed

--- run_query.py
+++ run_query.py
@@ -2,6 +2,7 @@ import argparse
 import requests
 import numpy as np
 
+
 def parse_args():
     # Parse command line arguments
     parser = argparse.ArgumentParser()
@@ -23,22 +24,22 @@ if __name__ == "__main__":
     args = parse_args()
 
     question_on_insurance = args.question
-    for p in np.linspace(0.70, 1.0, 10):
-        for t in np.linspace(0.2, 0.9, 10):
-            print(f"temp : {t} \n")
-            print(f"top p : {p} \n")
+    # for p in np.linspace(0.70, 1.0, 10):
+    #     for t in np.linspace(0.2, 0.9, 10):
+    #         print(f"temp : {t} \n")
+    #         print(f"top p : {p} \n")
 
-            payload = {
-                "question": question_on_insurance,
-                "token_max_length": 340,
-                "temperature": t,
-                "top_p": p,
-            }
+    payload = {
+        "question": question_on_insurance,
+        "token_max_length": 340,
+        "temperature": 0.744,
+        "top_p": 0.7,
+    }
 
-            response = requests.post("http://localhost:5000/generate", params=payload).json()
-            response = response['text']
-            before, sep, after = response.partition('SELECT')
-            query = sep + after
-            query = find_between(query, "SELECT", "###")
-            query = "SELECT" + query
-            print("Query: " + query + '\n')
+    response = requests.post("http://localhost:5000/generate", params=payload).json()
+    response = response['text']
+    before, sep, after = response.partition('SELECT')
+    query = sep + after
+    query = find_between(query, "SELECT", "###")
+    query = "SELECT" + query
+    print("Query: " + query + '\n')
