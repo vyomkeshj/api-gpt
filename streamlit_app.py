a41ec669c39a97deb58944ea1bfd364c533b5210
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-28 15:41:34 +0200

    html

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ def main():
     hide_menu_style = """
             <style>
             #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
             </style>
             """
     st.markdown(hide_menu_style, unsafe_allow_html=True)
