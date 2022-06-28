861a86bc7e5961ef7498fc105480081c1b1f4bf9
vyomkeshj <vyomkeshjha@yahoo.com>
2022-06-28 18:14:11 +0200

    html

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,8 +92,9 @@ def main():
 
                     # print(result.head(5))
 
-                    # question_col.dataframe(data=result, width=None, height=None)
-                    AgGrid(result)
+                    question_col.dataframe(data=result, width=None, height=None)
+                    # AgGrid(result)
+                    question_col.text(f"raw_output: {model_output}")
 
 
                     try_count = 0
@@ -105,9 +106,8 @@ def main():
                 question_col.markdown("Please try again with a slightly different question? :)", unsafe_allow_html=True)
             else:
                 question_col.text(f"Query done in {response['compute_time']:.3} s.")
-            new_hist = [question_on_insurance, model_output, str(successful_run)]
-            history = history.append(pd.Series(new_hist, index=history.columns[:len(new_hist)]), ignore_index=True)
-
+            my_dict = {'Query': question_on_insurance, 'Response': model_output, 'Correct': str(successful_run)}
+            history = history.append(my_dict, ignore_index=True)
             history.to_csv(HIST_CSV_FILE)
 
     if False:
